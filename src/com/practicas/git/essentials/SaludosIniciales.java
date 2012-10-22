05c99b3e5b983b7c38e050d026c1963df7820c19
Fabricio <fabricio.cejas@movile.com>
2012-10-22 15:28:20 -0300

    initial commit

---
+++ src/com/practicas/git/essentials/SaludosIniciales.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.practicas.git.essentials;
+
+/**
+ * @author Rodrigo
+ *
+ */
+public class SaludosIniciales {
+	
+	private static final String SALUDO_INICIAL = "Hola a todo el mundo!!!!!";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(SALUDO_INICIAL);
+
+	}
+
+}
